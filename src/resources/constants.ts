553cb86b54b397816bb572b61b048f885b65e1ec
Alex Mackenzie <AMackenzie@agily.local>
2017-01-20 17:57:42 +0100

    init commit (working)

---
+++ src/resources/constants.ts
@@ -0,0 +1,5 @@
+export const SITE_NAME_ABBR = 'MRT';
+export const SITE_NAME = 'Mutual Response Team';
+export const AYS = "Are you sure";
+export const AYS_DELETE = "Are you sure you want to delete";
+export const debugConsoleLog = true;
